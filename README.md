19a088e5723ea112e587d4395597fa679cd9a548
Michael Rimmer <na.michaeljrimmer@gmail.com>
2020-09-22 16:15:45 +0100

    Parse predictions file

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # heatmap
 
 ILP Coursework 1
+
+## Requirements
+
+1. parse input file
+2. create heatmap json
+3. generate relevant geojson using existing geojson of Edinburgh campus
+4. output heatmap.geojson to 'default output directory'
+
+## Unknowns
+
+3. Java
+    * read from input file
+    * output file
+    * where to put extra files like existing geojson
+    * how to handle errors
+    * junit testing
+    * make api request to render image?
--- src/main/java/uk/ac/ed/inf/heatmap/App.java
+++ src/main/java/uk/ac/ed/inf/heatmap/App.java
@@ -1,13 +1,44 @@
 package uk.ac.ed.inf.heatmap;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "First parameter is " + args[0] );
+        String[] predictions = parsePredictions("src/main/resources/predictions.txt");
+        System.out.println("The predictions are: " + Arrays.toString(predictions));
+    }
+
+    public static String[] parsePredictions(String fileName) {
+        String[] predictions = new String[100];
+
+        try {
+            File predictionsFile = new File(fileName);
+            Scanner fileReader = new Scanner(predictionsFile);
+            int i = 0;
+            while (fileReader.hasNextLine()) {
+              // each row expected to have 10 sensor predictions
+              String row = fileReader.nextLine();
+              String[] rowValues = row.split(",");
+              System.out.println(row);
+              for (int j = 0 ; j < 10; j++) {
+                  predictions[i] = rowValues[j];
+                  i++;
+              }
+            }
+            fileReader.close();
+
+
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+        return predictions;
     }
 }
